b6acc7cade9c89c11380352483586d0e9027460d
gustatarem <gustatarem@brq.com>
2022-03-25 16:19:03 -0300

    create xcode project

---
+++ globoplay-app-challenge/globoplay-app-challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  globoplay-app-challenge
+//
+//  Created by Gustavo Tatarem on 25/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
